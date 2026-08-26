4f4f92b3b3924ca50504f067655130e1ad5b94ae
ogrich221 <ogrichindeed@gmail.com>
2026-08-26 19:11:49 -0400

    feat: add banking admin dashboard for user activity oversight & balance alteration

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -378,6 +378,9 @@ function Footer() {
             <a href="#" className="hover:text-white">Terms</a>
             <a href="#" className="hover:text-white">Disclosures</a>
             <a href="#" className="hover:text-white">FDIC</a>
+            <Link to="/admin" className="font-semibold text-brand-orange hover:underline">
+              Admin Portal
+            </Link>
           </div>
         </div>
       </div>
